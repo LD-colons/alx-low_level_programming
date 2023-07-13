f5383632fe75bafb672a7064eef2849db7d8a776
LD-colons <libendy1@gmail.com>
2023-07-13 16:55:45 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+printf(sizeof(char));
+printf(sizeof(int));
+printf(sizeof(long int));
+printf(sizeof(long long int));
+printf(sizeof(float));
+	return (0);
+}
