f67aaf3db54b80059ae1ffc5191c80fe7a9f0bd2
LD-colons <libendy1@gmail.com>
2023-07-13 18:00:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of char: %lu bytes\n", (unsigned long)sizeof(c));
-printf("Size of int: %lu bytes\n", (unsigned long) sizeof(i));
-printf("Size of long int: %lu bytes\n", (unsigned long) sizeof(li));
-printf("Size of long long int: %lu bytes\n", (unsigned long) sizeof(lli));
-printf("Size of float: %lu bytes\n", (unsigned long) sizeof(f));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of int: %lu byte(s)\n", (unsigned long) sizeof(i));
+printf("Size of long int: %lu bytes(s)\n", (unsigned long) sizeof(li));
+printf("Size of long long int: %lu bytes(s)\n", (unsigned long) sizeof(lli));
+printf("Size of float: %lu bytes(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
