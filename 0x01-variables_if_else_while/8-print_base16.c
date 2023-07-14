062a63a0acebd3ad5ea4756bea4620ef37ce247e
LD-colons <libendy1@gmail.com>
2023-07-14 16:41:59 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char alpha;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + 0);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+}
