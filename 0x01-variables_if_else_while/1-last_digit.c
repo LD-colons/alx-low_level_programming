e94d1f7f7d1d65b77903c8923da87757c3f2e002
LD-colons <libendy1@gmail.com>
2023-07-14 14:19:27 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else if (ldigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, ldigit);
+		printf("Last digit of %d is %d and is 0\n", n, ldigit);
 	}
 	else
 		printf("Last digit of %d id %d and is less than 6 and not 0\n", n, ldigit);
