05fc8a39ac76f8b83895ebc324872ac722621290
LD-colons <libendy1@gmail.com>
2023-07-19 16:54:25 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of the number
+ * @n: the number those last digit is evaluated
+ * Return: the digit
+ */
+int print_last_digit(int n)
+{
+	int ldigit;
+
+	ldigit = n % 10;
+
+	if (ldigit < 0)
+	{
+		_putchar(-1 * ldigit);
+		return (ldigit);
+	}
+	else
+	{
+		_putchar(ldigit);
+		return (ldigit);
+	}
+	_putchar('\n');
+}
