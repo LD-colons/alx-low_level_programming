4a63eceea1ea70006725fdf86a062b551413749c
LD-colons <libendy1@gmail.com>
2023-07-20 16:07:22 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			puts("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			puts("Buzz");
+		}
+		else if (i % 15 == 0)
+		{
+			puts("FizzBuzz");
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+		puts(" ");
+	}
+	return (0);
+}
