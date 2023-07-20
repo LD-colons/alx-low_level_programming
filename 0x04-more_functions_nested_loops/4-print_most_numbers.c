8fafdc46a18f1e9d2a991e743ade57685b5a4725
LD-colons <libendy1@gmail.com>
2023-07-20 12:40:25 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a == 50 || a == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
