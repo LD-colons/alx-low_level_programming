5fb557f1d111da15a17b0fd98dc9451d7332f45c
LD-colons <libendy1@gmail.com>
2023-07-20 12:20:34 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print number from 0 through 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('$');
+	return (0);
+}
