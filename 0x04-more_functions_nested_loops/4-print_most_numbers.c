164ccb30468a836a7d90f9270392874a02bcb42d
LD-colons <libendy1@gmail.com>
2023-07-20 13:04:00 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-	char a;
+	int a;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		if (a != 50 || a != 52)
+		if (a != 2 || a != 4)
 		{
 			_putchar(a);
 		}
