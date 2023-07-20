2578966afc0ed921ed0f1c09a7ab02054bb3bd56
LD-colons <libendy1@gmail.com>
2023-07-20 13:14:45 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print number 0 to 14 ten times
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
