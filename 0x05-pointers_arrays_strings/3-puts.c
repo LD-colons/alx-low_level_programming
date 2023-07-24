afed910d4bc5f315871781cb0eff8b71b23926b9
LD-colons <libendy1@gmail.com>
2023-07-24 13:00:00 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - function print string followed by new line
+ * @str: pointer as parameter
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+}
