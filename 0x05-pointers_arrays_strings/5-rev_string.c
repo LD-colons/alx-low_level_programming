0679a37f9da46792e1cc47fa1f2507a90715aa4f
LD-colons <libendy1@gmail.com>
2023-07-25 04:36:22 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: parameter it takes
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int count = 0;
+	int rev;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	s--;
+	for (rev = count; rev > 0; rev--)
+	{
+		_putchar(*s);
+		s--;
+	}
+}
