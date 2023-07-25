85f8105a8dd2f938e211d7678db480911b1fbfe8
LD-colons <libendy1@gmail.com>
2023-07-25 06:17:07 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - print half of string
+ * @str: parameter it takes
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int count;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	int i;
+
+	if (count % 2 == 0)
+	{
+		i = count;
+	}
+	else
+	{
+		i = count - 1;
+	}
+	int j;
+
+	for (j = i / 2; j <= count; j++)
+	{
+		_putchar(*(str + j));
+		str++;
+	}
+	_putchar('\n');
+}
