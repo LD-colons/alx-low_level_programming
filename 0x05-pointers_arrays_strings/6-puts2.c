49e29b22c352a257d7bd46baa12a97d2bde5d90c
LD-colons <libendy1@gmail.com>
2023-07-25 05:06:10 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of string
+ * starting with the first character
+ * @str: parameter it takes
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
