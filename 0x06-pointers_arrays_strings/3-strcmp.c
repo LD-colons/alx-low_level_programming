f01557a0894b538826109ea13fac47734a6bc759
LD-colons <libendy1@gmail.com>
2023-07-26 13:01:40 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strcmp - compares two string
+ * @s1: parameter it takes
+ * @s2: parameter it takes
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int flag = 0;
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] > s2[i])
+			{
+				flag = 15;
+				break;
+			}
+			else
+			{
+				flag = -15;
+				break;
+			}
+		}
+	}
+	return (flag);
+}
+
+
