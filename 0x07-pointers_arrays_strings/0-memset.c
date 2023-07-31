4703b7655c0d23586fd45d9a9e6d1dc308da5599
LD-colons <libendy1@gmail.com>
2023-07-31 06:47:57 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memset - fills the memory with constant byte
+ * @s: memory to be filled
+ * @b: constant byte fills in
+ * @n: number that fills to
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n * sizeof(b); i++)
+	{
+		s[i] = b;
+	}
+	s[i] = '\0';
+	return (s);
+}
