4705219b747546175ac425f8584131d345d0e22b
LD-colons <libendy1@gmail.com>
2023-07-31 08:21:26 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,15 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0;  i < n; i++)
+	for (i = 0;  i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (dest[i] != '\0')
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
