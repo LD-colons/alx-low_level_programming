a664e4329102ee0515e91ea78a6e90ef7f7e6277
LD-colons <libendy1@gmail.com>
2023-08-02 06:10:01 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,14 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
 
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
+		_putchar('\n');
 	}
 	else
 	{
-		_putchar(s[i]);
-		i = i + 1;
-		_puts_recursion(s[i]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
