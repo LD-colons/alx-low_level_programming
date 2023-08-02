dbeae324f3e5d7a36cd566c6a8bda4db0ea1c08c
LD-colons <libendy1@gmail.com>
2023-08-02 05:32:00 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string followed by newline
+ * @s: string parameter
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		s[i] = '\n';
+		_putchar(s[i]);
+	}
+	else
+	{
+		_putchar(s[i]);
+		i = i + 1;
+		_puts_recursion(&s[i]);
+	}
+}
