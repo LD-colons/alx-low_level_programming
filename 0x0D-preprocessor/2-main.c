670a5cf5b7f94db9749c6c8c73a882c2c2c6efae
LD-colons <libendy1@gmail.com>
2023-08-14 11:55:35 -0700

    4-sum.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * main - where the program begin
@@ -7,7 +6,7 @@
  */
 int main(void)
 {
-	_putchar(__FILE__);
+	_putchar(__FILE_);
 	_putchar('\n');
 	return (0);
 }
