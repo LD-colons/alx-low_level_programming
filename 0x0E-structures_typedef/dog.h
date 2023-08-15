f194ef43ca1e45abae27c00e2cd90e83a157ca4a
LD-colons <libendy1@gmail.com>
2023-08-15 13:24:32 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef SUM
+#define SUM
+#include <stdio.h>
+/**
+ * struct dog - attributes of dog
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
