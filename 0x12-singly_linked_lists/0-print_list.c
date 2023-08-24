feb3013d58b08622a556da3aecd91034efcb37c7
LD-colons <libendy1@gmail.com>
2023-08-24 15:48:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,16 @@ size_t print_list(const list_t *h)
 
 	while (ptr != NULL)
 	{
-		count++;
 		if (ptr->str == NULL)
-			printf("[0] (nil)");
+		{
+			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%d]%s\n", ptr->len, ptr->str);
+		{
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		}
+		count++;
 		ptr = ptr->next;
 	}
-	return (count);
+		return (count);
 }
