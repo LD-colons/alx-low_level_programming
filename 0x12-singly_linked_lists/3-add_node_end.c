23da0c4bb19fd03329dd5f4d4178d541d5d0041d
LD-colons <libendy1@gmail.com>
2023-08-26 06:59:07 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * add_node - the function that add new node at the end
+ * add_node_end - the function that add new node at the end
  * @head: it where the node starts
  * @str: it the value of nodes
  * Return: address of new element or NULL
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newnode = malloc(sizeof(list_t));
 
