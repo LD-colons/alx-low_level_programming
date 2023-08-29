f93a76e6313ee55a636ee3e3497a7532069cc459
LD-colons <libendy1@gmail.com>
2023-08-29 02:48:16 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,10 +12,20 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (temp == NULL)
 		return (NULL);
-	while (ptr->next != NULL)
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL)
 	{
-		ptr = ptr->next;
+		*head = temp;
+		return (temp);
 	}
-	ptr->next = temp;
+	else
+	{
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = temp;
+	}
 	return (temp);
 }
