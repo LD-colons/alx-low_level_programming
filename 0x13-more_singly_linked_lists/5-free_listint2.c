3eaaa83ad2f674549f317f97b74b94110114f4ff
LD-colons <libendy1@gmail.com>
2023-08-29 05:04:58 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - free the linked list
+ * @head: head of the linked list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *freenode = *head;
+
+	while (freenode != NULL)
+	{
+		listint_t *next = freenode->next;
+
+		free(freenode);
+		freenode = next;
+	}
+	*head = NULL;
+}
